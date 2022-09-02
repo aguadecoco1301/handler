5d8994a9eaf9631f8c0c65213338a0272961d320
adr4142 <danielaulloa.000@gmail.com>
2022-09-02 01:42:53 -0300

    Corregido el bug de message.lang

--- commands/example/index.js
+++ commands/example/index.js
@@ -1,7 +1,7 @@
 //Hello! You can copy and paste this folder to
 //fast create a command
 exports.run = (app, message, args) => {
-	message.reply(message.lang({
+	message.reply(app.lang({
 		en: "Hello world",
 		es: "Hola mundo",
 	}))
